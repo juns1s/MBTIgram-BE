3f5b22975085871b860713225fe71639d889eb59
juns1s <juns1s@naver.com>
2023-07-11 12:02:17 -0700

    git init

---
+++ MBTInstagram/src/main/java/com/Zizon/MBTInstagram/MbtInstagramApplication.java
@@ -0,0 +1,13 @@
+package com.Zizon.MBTInstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtInstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtInstagramApplication.class, args);
+	}
+
+}
---
+++ MBTInstagram/src/test/java/com/Zizon/MBTInstagram/MbtInstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zizon.MBTInstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtInstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
