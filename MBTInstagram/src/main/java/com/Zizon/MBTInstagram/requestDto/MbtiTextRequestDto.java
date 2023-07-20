587818e234f1836a7ed5a398cf871acc2ee101b0
juns1s <juns1s@naver.com>
2023-07-20 10:54:51 -0700

    feat: text분석을 위한 DTO구현

---
+++ MBTInstagram/src/main/java/com/Zizon/MBTInstagram/requestDto/MbtiTextRequestDto.java
@@ -0,0 +1,13 @@
+package com.Zizon.MBTInstagram.requestDto;
+
+public class MbtiTextRequestDto {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
