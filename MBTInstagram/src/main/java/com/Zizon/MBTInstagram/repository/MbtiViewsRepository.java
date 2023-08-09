3f67e545a26197eaac3be13a6e167b49eb67e70a
juns1s <juns1s@naver.com>
2023-08-09 16:43:59 +0900

    feat: 단일 mbti조회 구현

--- MBTInstagram/src/main/java/com/Zizon/MBTInstagram/repository/MbtiViewsRepository.java
+++ MBTInstagram/src/main/java/com/Zizon/MBTInstagram/repository/MbtiViewsRepository.java
@@ -1,14 +1,17 @@
 package com.Zizon.MBTInstagram.repository;
 
 import com.Zizon.MBTInstagram.domain.MbtiViews;
+import com.Zizon.MBTInstagram.global.MbtiType;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Repository
 public interface MbtiViewsRepository extends JpaRepository<MbtiViews, String> {
 
-    public ArrayList<MbtiViews> findAll(Sort sort);
+    ArrayList<MbtiViews> findAll(Sort sort);
+    Optional<MbtiViews> findByType(MbtiType mbtiType);
 }
