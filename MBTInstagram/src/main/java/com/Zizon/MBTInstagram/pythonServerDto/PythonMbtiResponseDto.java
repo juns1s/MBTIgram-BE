670e3b8269c4de78325ec1cd6c26e3db3c836ea6
juns1s <juns1s@naver.com>
2023-08-11 16:00:26 +0900

    refactor: 패키지 명 수정

--- MBTInstagram/src/main/java/com/Zizon/MBTInstagram/pythonServerDto/PythonMbtiResponseDto.java
+++ MBTInstagram/src/main/java/com/Zizon/MBTInstagram/pythonServerDto/PythonMbtiResponseDto.java
@@ -1,11 +1,11 @@
-package com.Zizon.MBTInstagram.flaskDto;
+package com.Zizon.MBTInstagram.pythonServerDto;
 
 import lombok.Data;
 
 import java.util.Map;
 
 @Data
-public class FlaskResponseDto {
+public class PythonMbtiResponseDto {
 
     private String mbti;
     private Map<String, Double> prob;
